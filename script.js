ab11849e6c1c1e67eaf30cd88f0ea8c35d6845cc
Ayo Omotosho <aomotosho4@gmail.com>
2024-02-19 18:38:12 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const text = document.querySelector(".text");
+
+letters.forEach((letter) => {
+  const span = document.createElement("span");
+  span.textContent = letter;
+  text.appendChild(span);
+});
